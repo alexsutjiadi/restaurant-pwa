a17d88b57dcc71bb36845f53dc50854dc9f81e25
alexsutjiadi <37326814+alexsutjiadi@users.noreply.github.com>
2020-04-26 16:25:18 +0700

    Proto 1.1
    + finish add update delete
    + service worker, manifest, dll

---
+++ tes/service-worker.js
@@ -0,0 +1,42 @@
+const FILES_TO_CACHE = [ 'offline.html', ];
+const CACHE_NAME="namaCache";
+self.addEventListener('activate', (evt) => {
+	evt.waitUntil( 
+    caches.keys().then((keyList) => { 
+        return Promise.all(keyList.map((key) => { 
+            if (key !== CACHE_NAME) { 
+                console.log('[ServiceWorker] Removing old cache', key); 
+                return caches.delete(key); 
+            } 
+        }));
+    })
+);
+
+});
+
+self.addEventListener('fetch', (evt) => {
+
+evt.respondWith( 
+    fetch(evt.request) 
+        .catch(() => { 
+            return caches.open(CACHE_NAME) 
+                .then((cache) => { 
+                    return cache.match('offline.html'); 
+                }); 
+        }) 
+	); 
+
+});
+
+self.addEventListener('install', (evt) => {
+   evt.waitUntil( 
+     caches.open(CACHE_NAME).then((cache) => { 
+         console.log('[ServiceWorker] Pre-caching offline page'); 
+         return cache.addAll(FILES_TO_CACHE);
+     }) 
+  );
+ self.skipWaiting();
+
+     
+
+});
